254f858c8c157b775dc3922abbbacc1045587544
Maiquel Knechtel <maiquelknechtel@MacBook-Pro-de-Maiquel.local>
2018-06-30 14:54:58 -0300

    get and set Mensagem

---
+++ client/js/app/models/Mensagem.js
@@ -0,0 +1,18 @@
+class Mensagem {
+
+    constructor() {
+
+      this._texto;
+    }
+
+    get texto() {
+
+        return this._texto;
+
+    }
+
+    set texto(texto){
+        return this._texto=texto;
+    }
+   
+}
